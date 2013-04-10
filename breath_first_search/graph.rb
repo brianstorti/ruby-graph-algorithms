213136c5c040dce55cf34b944ff75f9606f2bfcd
Brian Thomas Storti <btstorti@gmail.com>
2013-04-10 01:21:33 -0300

    add comments

--- breath_first_search/graph.rb
+++ breath_first_search/graph.rb
@@ -9,6 +9,8 @@ class Graph
 
   # We are dealing with an undirected graph,
   # so I increment the "adjacents" in both sides.
+  # The breath first will work the same way with
+  # a directed graph.
   def add_edge(node_a, node_b)
     node_a.adjacents << node_b
     node_b.adjacents << node_a
--- depth_first_search/graph.rb
+++ depth_first_search/graph.rb
@@ -7,6 +7,10 @@ class Graph
     @nodes << node
   end
 
+  # We are dealing with an undirected graph,
+  # so I increment the "adjacents" in both sides.
+  # The depth first will work the same way with
+  # a directed graph.
   def add_edge(node_a, node_b)
     node_a.adjacents << node_b
     node_b.adjacents << node_a
