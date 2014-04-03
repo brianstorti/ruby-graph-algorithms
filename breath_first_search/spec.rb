927f18629e1cba4cbfe935cfb46f42608e8e2cbd
Brian Thomas Storti <btstorti@gmail.com>
2014-04-02 23:50:16 -0300

    create automated test for the bfs

---
+++ breath_first_search/spec.rb
@@ -0,0 +1,28 @@
+require 'minitest/autorun'
+
+require_relative "graph"
+require_relative "node"
+require_relative "breath_first_search"
+
+describe BreathFirstSearch do
+  before do
+    @node1 = Node.new("Node #1")
+    @node2 = Node.new("Node #2")
+    @node3 = Node.new("Node #3")
+    @node4 = Node.new("Node #4")
+    @node5 = Node.new("Node #5")
+  end
+
+  it 'finds the shortest path to a node' do
+    graph = Graph.new
+    graph.add_edge(@node1, @node2)
+    graph.add_edge(@node2, @node3)
+    graph.add_edge(@node2, @node4)
+    graph.add_edge(@node4, @node5)
+    graph.add_edge(@node1, @node5)
+
+    path = BreathFirstSearch.new(graph, @node1).shortest_path_to(@node5)
+
+    path.must_equal [@node1, @node5]
+  end
+end
