a342d494278f9eb62e8b9af9a6e7b19aafea5f10
vinney cavallo <vcavallo@gmail.com>
2015-12-22 11:59:28 -0500

    Fix all misspelled instances of 'breath'

--- breadth_first_search/graph.rb
+++ breadth_first_search/graph.rb
@@ -2,7 +2,7 @@ class Graph
 
   # We are dealing with an undirected graph,
   # so I increment the "adjacents" in both sides.
-  # The breath first will work the same way with
+  # The breadth first will work the same way with
   # a directed graph.
   def add_edge(node_a, node_b)
     node_a.adjacents << node_b
